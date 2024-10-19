4ced06ae5d6701495e2c5f9482e171615716838a
veera8332 <52772871+veera8332@users.noreply.github.com>
2024-10-19 15:57:28 +0530

    first push

---
+++ SampleProject/src/test/java/pratice/CreateContactTest.java
@@ -0,0 +1,16 @@
+package pratice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+	@Test
+	public void createContactTest() {
+		System.out.println("createContact");	
+	}
+	@Test
+	public void deleteContactTest() {
+		System.out.println("deleteContact");	
+	}
+
+}
---
+++ SampleProject/src/test/java/pratice/CreateOrgTest.java
@@ -0,0 +1,16 @@
+package pratice;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("createOrg");	
+	}
+	@Test
+	public void deleteOrgTest() {
+		System.out.println("deleteOrg");	
+	}
+
+}
